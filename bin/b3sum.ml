5f8a15c2f389b0c21721a846a37484a6defc567d
Rudi Grinberg <me@rgrinberg.com>
2025-04-29 12:53:58 +0100

    chore: ocamlformat
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- bin/b3sum.ml
+++ bin/b3sum.ml
@@ -1,8 +1,7 @@
 let () =
   let file = Sys.argv.(1) in
-  let fd = Unix.openfile file [O_RDONLY] 0 in
+  let fd = Unix.openfile file [ O_RDONLY ] 0 in
   let hash = Blake3_mini.fd fd in
   Unix.close fd;
   print_endline (Blake3_mini.Digest.to_hex hash)
 ;;
-
