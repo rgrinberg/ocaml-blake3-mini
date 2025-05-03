a55d0f3188eea252b6cbf6c864899ba8e8fce9ec
Ali Caglayan <alizter@gmail.com>
2025-05-03 19:06:23 +0200

    nix: flake with ocaml versions and formatting
    Signed-off-by: Ali Caglayan <alizter@gmail.com>
    
    <!-- ps-id: 5e2ce0d5-8955-4a8a-8595-27d8a0d3f29a -->

--- flake.nix
+++ flake.nix
@@ -3,45 +3,77 @@
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:nixos/nixpkgs";
-  inputs.dune.url = "github:alizter/dune/foreign_objects";
+  inputs.dune.url = "github:ocaml/dune";
 
-  outputs = { self, nixpkgs, flake-utils, dune }:
-  flake-utils.lib.eachDefaultSystem (system:
-  let
-    pkgs =
-      nixpkgs.legacyPackages.${system}.appendOverlays [
-        (self: super: {
-          ocamlPackages = super.ocaml-ng.ocamlPackages.overrideScope (oself: osuper: {
-            dune_3 = dune.packages.${system}.default;
-          });
-        })
-      ];
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      dune,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
 
-    inherit (pkgs.ocamlPackages) buildDunePackage;
-  in
-  rec {
-    packages = rec {
-      default = ocaml-blake3-mini;
-      ocaml-blake3-mini = buildDunePackage {
-        pname = "ocaml-blake3-mini";
-        version = "n/a";
-        src = ./.;
-        duneVersion = "3";
-        propagatedBuildInputs = [];
-        checkInputs = with pkgs.ocamlPackages; [
-          ppx_inline_test
-          ppx_expect
-        ];
-        doCheck = true;
-      };
-    };
-    devShells.default = pkgs.mkShell {
-      inputsFrom = pkgs.lib.attrValues packages;
-      buildInputs = with pkgs.ocamlPackages; [
-        pkgs.ccls
-        pkgs.ocamlformat
-        ocaml-lsp
-      ];
-    };
-  });
+        applyDuneOverride =
+          ocamlPackages_set:
+          ocamlPackages_set.overrideScope (oself: osuper: { dune_3 = dune.packages.${system}.default; });
+
+        buildMyPackage =
+          { ocamlPackages }:
+          ocamlPackages.buildDunePackage {
+            pname = "ocaml-blake3-mini";
+            version = "n/a";
+            src = ./.;
+            duneVersion = "3";
+            propagatedBuildInputs = [ ];
+            checkInputs = with ocamlPackages; [
+              ppx_inline_test
+              ppx_expect
+            ];
+            doCheck = true;
+          };
+
+        setupDevShell =
+          { pkgs, ocamlPackages }:
+          pkgs.mkShell {
+            inputsFrom = [ (buildMyPackage { inherit ocamlPackages; }) ];
+            buildInputs = with ocamlPackages; [
+              ocaml-lsp
+              ocamlformat
+              pkgs.ccls
+            ];
+          };
+
+      in
+      {
+        packages = rec {
+          default = buildMyPackage {
+            ocamlPackages = applyDuneOverride pkgs.ocamlPackages;
+          };
+          ocaml-blake3-mini = default;
+        };
+
+        devShells = {
+          default = setupDevShell {
+            inherit pkgs;
+            ocamlPackages = applyDuneOverride pkgs.ocamlPackages;
+          };
+
+          ocaml_4_14 = setupDevShell {
+            inherit pkgs;
+            ocamlPackages = applyDuneOverride pkgs.ocaml-ng.ocamlPackages_4_14;
+          };
+
+          ocaml_4_08 = setupDevShell {
+            inherit pkgs;
+            ocamlPackages = applyDuneOverride pkgs.ocaml-ng.ocamlPackages_4_08;
+          };
+        };
+
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
 }
